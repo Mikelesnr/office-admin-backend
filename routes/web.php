88de47199923880f7e9b7df61da739aca16d7e38
Michael Mwanza <mwanza.n.m@gmail.com>
2025-09-10 23:27:21 +0200

    login fix

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cookie;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+Route::get('/csrf-token', function () {
+    // Trigger Sanctum to set the CSRF cookie
+    \Illuminate\Support\Facades\Route::dispatch(
+        \Illuminate\Http\Request::create('/sanctum/csrf-cookie', 'GET')
+    );
+
+    // Read the token from the cookie
+    $token = Cookie::get('XSRF-TOKEN');
+
+    return response()->json([
+        'token' => $token,
+    ]);
+});
